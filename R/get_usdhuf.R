4517b009c9064d12401201201caa61ed5a538dfa
Ozan <semihozankaya@gmail.com>
2021-05-18 22:21:26 +0200

    adding historical rates

--- R/get_usdhuf.R
+++ R/get_usdhuf.R
@@ -21,3 +21,40 @@ get_usdhuf <- function(retried = 0) {
   log_info('1 USD = {forint(usdhuf)}')
   usdhuf
 }
+
+#' Gets the historical USD/HUF rate
+#' @param start_date starting date to receive exchange rates information
+#' @param end_date the last date for our exchange rates
+#' @inheritParams get_usdhuf
+#' @return \code{data.table} object
+#' @export
+#' @importFrom checkmate assert_data_table
+#' @importFrom logger log_error log_info
+#' @importFrom data.table data.table
+#' @importFrom httr GET content
+#' @examples
+#' get_usdhuf()
+
+get_usdhufs <- function(start_date = Sys.Date() - 30, end_date = Sys.Date(), retried = 0) {
+  tryCatch({
+    usdhuf <- response <- GET(
+      "https://api.exchangerate.host/timeseries",
+      query = list(
+        start_date = start_date,
+        end_date = end_date,
+        base = "USD",
+        symbols = "HUF"
+      )
+    )
+    exchange_rates <- content(response)$rates
+    usdhufs <- data.table(date = as.Date(names(exchange_rates)),
+                                 usdhuf = as.numeric(unlist(exchange_rates)))
+    assert_data_table(usdhufs, any.missing = FALSE)
+  }, error = function(e) {
+    log_error(e$message)
+    Sys.sleep(1 + retried ^ 2)
+    get_usdhufs(start_date = start_date, end_date = end_date, retried = retried + 1)
+  })
+  log_info('The average USD/HUF rate over the last {end_date-start_date + 1} days is {forint(mean(usdhufs$usdhuf))}')
+  usdhufs
+}
